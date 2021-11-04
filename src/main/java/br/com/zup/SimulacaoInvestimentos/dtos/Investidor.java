c387f0b544999021d631958e0dcaf980970eb811
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-11-04 11:13:48 -0300

    Criação classe Investidor

---
+++ src/main/java/br/com/zup/SimulacaoInvestimentos/dtos/Investidor.java
@@ -0,0 +1,10 @@
+package br.com.zup.SimulacaoInvestimentos.dtos;
+
+public class Investidor {
+
+    private String nome;
+    private String email;
+    private String cpf;
+    private double valorPrevisto;
+    private int periodoDeAplicacaoMeses;
+}
---
+++ src/main/java/br/com/zup/SimulacaoInvestimentos/dtos/Risco.java
@@ -0,0 +1,4 @@
+package br.com.zup.SimulacaoInvestimentos.dtos;
+
+public enum Risco {
+}
