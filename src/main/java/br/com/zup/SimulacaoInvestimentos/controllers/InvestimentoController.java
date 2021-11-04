264297ce4c0b6b1a88d608f36c943eee161f93fe
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-11-04 11:58:48 -0300

    Modelar classe InvestimentoDto

--- src/main/java/br/com/zup/SimulacaoInvestimentos/controllers/InvestimentoController.java
+++ src/main/java/br/com/zup/SimulacaoInvestimentos/controllers/InvestimentoController.java
@@ -1,8 +1,25 @@
 package br.com.zup.SimulacaoInvestimentos.controllers;
 
+import br.com.zup.SimulacaoInvestimentos.dtos.InvestimentoDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InvestimentoController {
+
+    @PostMapping
+    public InvestimentoDto cadastrarInvestimento(@RequestBody InvestimentoDto investimentoDto){
+        InvestimentoDto dadosInvestimento = new InvestimentoDto();
+        return dadosInvestimento;
+    }
+
+    @GetMapping
+    public InvestimentoDto simularinvestimento(InvestimentoDto investimentoDto){
+
+        return null;
+
+    }
 
 }
--- src/main/java/br/com/zup/SimulacaoInvestimentos/dtos/InvestimentoDto.java
+++ src/main/java/br/com/zup/SimulacaoInvestimentos/dtos/InvestimentoDto.java
@@ -7,5 +7,65 @@ public class InvestimentoDto {
     private String cpf;
     private double valorPrevisto;
     private int periodoDeAplicacaoMeses;
-    private RiscoDto nivelDoRisco;
+    private RiscoDto risco;
+
+    public InvestimentoDto() {
+    }
+
+    public InvestimentoDto(String nome, String email, String cpf, double valorPrevisto, int periodoDeAplicacaoMeses, RiscoDto risco) {
+        this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+        this.valorPrevisto = valorPrevisto;
+        this.periodoDeAplicacaoMeses = periodoDeAplicacaoMeses;
+        this.risco = risco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getValorPrevisto() {
+        return valorPrevisto;
+    }
+
+    public void setValorPrevisto(double valorPrevisto) {
+        this.valorPrevisto = valorPrevisto;
+    }
+
+    public int getPeriodoDeAplicacaoMeses() {
+        return periodoDeAplicacaoMeses;
+    }
+
+    public void setPeriodoDeAplicacaoMeses(int periodoDeAplicacaoMeses) {
+        this.periodoDeAplicacaoMeses = periodoDeAplicacaoMeses;
+    }
+
+    public RiscoDto getRisco() {
+        return risco;
+    }
+
+    public void setRisco(RiscoDto risco) {
+        this.risco = risco;
+    }
 }
